b15d9ec2fbb07f4ddb35d6f2b2488de663511f04
taaraq-ahmed <taaraqahmed@gmail.com>
2024-11-21 13:28:56 +0600

    fix isssue

--- components/AfzalImdad/index.js
+++ components/AfzalImdad/index.js
@@ -1,16 +1,9 @@
 import { useEffect, useRef, useState } from "react";
-import A from "./afzalImdadSVGs/A";
-import B from "./afzalImdadSVGs/B";
-import E1 from "./afzalImdadSVGs/E1";
-import E2 from "./afzalImdadSVGs/E2";
-import H from "./afzalImdadSVGs/H";
+
 import K1 from "./afzalImdadSVGs/K1";
 import K2 from "./afzalImdadSVGs/K2";
 import K3 from "./afzalImdadSVGs/K3";
-import M from "./afzalImdadSVGs/M";
-import N from "./afzalImdadSVGs/N";
-import N2 from "./afzalImdadSVGs/N2";
-import U from "./afzalImdadSVGs/U";
+
 
 const SvgWrapper = ({ SVG, className, ...rest }) => (
   <div
@@ -44,30 +37,6 @@ export default function AfzalImdad({ }) {
   return (
     <div ref={ref} className={`ai-afzal-imdad ${mount ? 'ai-afzal-imdad-visible' : ''}`}>
       <SvgWrapper
-        className="ai_svg_M"
-        SVG={M}
-      />
-      <SvgWrapper
-        className="ai_svg_U"
-        SVG={U}
-      />
-      <SvgWrapper
-        className="ai_svg_N"
-        SVG={N}
-      />
-      <SvgWrapper
-        className="ai_svg_E1"
-        SVG={E1}
-      />
-      <SvgWrapper
-        className="ai_svg_E2"
-        SVG={E2}
-      />
-      <SvgWrapper
-        className="ai_svg_B"
-        SVG={B}
-      />
-      <SvgWrapper
         className="ai_svg_K1"
         SVG={K1}
       />
@@ -78,18 +47,6 @@ export default function AfzalImdad({ }) {
       <SvgWrapper
         className="ai_svg_K3"
         SVG={K3}
-      />
-      <SvgWrapper
-        className="ai_svg_H"
-        SVG={H}
-      />
-      <SvgWrapper
-        className="ai_svg_A"
-        SVG={A}
-      />
-      <SvgWrapper
-        className="ai_svg_N2"
-        SVG={N2}
       />
     </div>
   )
